5c9a8e26d3c1f14fdc8083edf33e0191e2829c00
ghofuragus <ghofuragus444@gmail.com>
2022-12-22 13:34:22 +0000

    Deploy gh-pages

---
+++ _next/static/XUX45AmxvAjxpYT8JmuZZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fcss\u002F8aefbc8ea163aed7.css","static\u002Fchunks\u002Fpages\u002Findex-efb6c6d38dfb5a68.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-75f8c62d1b846d4f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
